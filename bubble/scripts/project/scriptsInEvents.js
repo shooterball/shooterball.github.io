cf7f19b80a26d9955decb545a9c4235bbc8ee44f
shooterball <132680405+shooterball@users.noreply.github.com>
2023-09-27 16:22:07 +0300

    Add files via upload

---
+++ bubble/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Loadingsettings_Event2_Act8(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#1ec8fe'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
